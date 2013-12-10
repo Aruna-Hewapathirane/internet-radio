252022338af024524557e2c7d8c7db742ccd9175
Aruna-Hewapathirane <aruna.hewapathirane@gmail.com>
2013-12-09 22:23:12 -0500

    Update internet-radio.c

--- internet-radio.c
+++ internet-radio.c
@@ -9,7 +9,7 @@ int main ()
 // uses mplayer
 // char string[]="mplayer 'http://stream-93.shoutcast.com:80'" ;	
 
-// Uses gstreamer-tools
+// Uses gstreamer-tools this is how NOT to do things :-)
 char string[]="gst-launch playbin2 uri='http://stream-93.shoutcast.com:80'"; 
 
      system(string);
